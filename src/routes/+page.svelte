95f861df5eb02051a89c2a9af187e1e94a4e5b28
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-18 09:07:17 +0200

    rsetgsrdhgrs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import HabitContainer from "../components/habitContainer.svelte";
+   </script>
+   
+   
+   
+   
+   <div class="main flex justify-center">
+       <HabitContainer />
+   </div>
