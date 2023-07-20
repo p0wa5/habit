212b3706a7f044789ad1144f16cdb3645c2cae91
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-20 15:19:14 +0200

    work on load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 /** @type {import('./$types').PageServerLoad} */
-export async function load({}) {
+export async function load() {
     return {
         Habits: await prisma.habit.findMany( ) 
     };
