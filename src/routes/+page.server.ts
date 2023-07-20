d1cef83cdea90ab3b38e0d724da61a125d5b6b67
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-20 10:24:56 +0200

    change loadfunction return value to object which holds arrs

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,10 +5,6 @@ const prisma = new PrismaClient()
 /** @type {import('./$types').PageServerLoad} */
 export async function load({}) {
     return {
-        post: await prisma.habit.findUnique({
-            where: {
-                id: 1
-            }
-        }) 
+        post: await prisma.habit.findMany( ) 
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
     import HabitContainer from "../components/habitContainer.svelte";
     export let data;
-    console.log(data);
-</script>
-   
-   
-<p>{data.post?.name}</p>
-<p>{data.post?.description}</p>
-<p>{data.post?.streak}</p>
-<p>{data.post?.interval}</p>
-   
+    let habits = data; 
+
+
     
+</script>
+
    <div class="main flex justify-center">
        <HabitContainer />
    </div>
