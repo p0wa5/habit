8d981307a08f34d3797418b502271a64362b1164
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-20 10:40:51 +0200

    checkpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,13 @@
     import HabitContainer from "../components/habitContainer.svelte";
     export let data;
     let habits = data; 
+    export let habitsArr = [];
 
+
+    for(let i = 0; i < habits.post.length; i++){
+        console.log(habits.post[i])
+        habitsArr.push(habits.post[i])
+    }
 
     
 </script>
