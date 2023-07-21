2d0b8d0c68c150282fc4f93a866de379f6d31500
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-21 11:24:53 +0200

    try to add delete functions

--- src/components/habitContainer.svelte
+++ src/components/habitContainer.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
     import type { Habit } from "@prisma/client";
+    import { deleteHabit } from "../main"
     export let habitsArr: Habit[]
 </script>
 
 
 <div class="flex flex-col">
     {#each habitsArr as habit, i}
-        <p>{habitsArr[i].name}</p>
+        <div class="border">
+            <p>{habitsArr[i].name}</p>
+            <p>streak: {habitsArr[i].streak}</p>
+            <button on:click={deleteHabit(1)}>x</button>
+        </div>
     {/each}
 </div>
