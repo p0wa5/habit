ca161a8fc488c7abf358415856531bfa5e87956f
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-26 09:48:08 +0200

    add post endpoint

---
+++ src/routes/api/increaseStreak/+server.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import type { RequestEvent } from "./$types";
+
+const prisma = new PrismaClient();
+
+export async function POST({ request }: RequestEvent) {
+    const json = await request.json();
+    try {
+        await prisma.habit.update({
+            where: {
+                id: json.id  
+            },
+            data: {
+                streak: json.streakValue 
+            }
+        });
+        return new Response("deleted");
+    } catch (error) {
+        console.log(error);
+    }
+}
