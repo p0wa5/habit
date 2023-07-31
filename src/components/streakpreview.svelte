7d64930f7f6a660f3fc14875d1e0f233ccaeffe8
j.freiwald <j.freiwald@l-shop-team.de>
2023-07-31 16:05:41 +0200

    idk

---
+++ src/components/streakpreview.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+
+<div class="">
+    <div class="border w-40 h-10 p-1 m-1">
+        <div class="flex justify-between">
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+        </div>
+        <div class="flex justify-between mt-1">
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+            <div class="h-3 w-3 border"></div>
+        </div>
+    </div>
+</div>
